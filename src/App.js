7b92ac16cddc44d11415ade7dd29b42cc46e85cc
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-01 20:25:36 -0600

    Getting response of all the films.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+     this.state = {
+       data: []
+     }
+  }
 
   componentDidMount(){
-    fetch("https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49")
-    .then(data => { data.json()
-    console.log(data)
-  })
+    this.getMovies();
+  }
+
+  getMovies(){
+    fetch("https://ghibliapi.herokuapp.com/films", {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(response => { this.setState({data: response})});
+  }
+
+  allFilms(){
+
   }
 
   render() {
+    console.log(this.state.data)
     return (
       <div>
         <header>
-
+         <h1></h1>
         </header>
       </div>
     );
