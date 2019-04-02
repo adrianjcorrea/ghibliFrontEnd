c63e5c2e4845c6dc058c4ebbe782270858bb5391
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-02 16:03:26 -0600

    Configured search filter to title.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,30 @@
     constructor(){
       super();
        this.state = {
-         films: []
+         //Saving api response obj in films array.
+         films: [],
+         searchfield: ''
        }
     }
 
     //Mounts all films.
     componentDidMount(){
+      //Accesing Api ang getting response using fetch method
       fetch("https://ghibliapi.herokuapp.com/films")
       .then(response => response.json())
+      //Setting Response to state.films
       .then(response => { this.setState({films: response})});
     }
 
     render() {
-      const { films } = this.state
+      //Destructuring to use state.
+      const { films } = this.state;
       const filteredFilms = films.filter(film => {
-        return film.title
+        //Will search by title name.
+        return film.title.toLowerCase().includes(this.state.searchfield.toLowerCase());
       })
+      //While films load will display Loading.
+      //When films are captured in state then will display app compontets.
       return !films.length ?
       <h1 className='tc'>Loading</h1> :
 
