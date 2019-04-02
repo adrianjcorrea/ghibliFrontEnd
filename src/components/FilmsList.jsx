3c691e4d7efe969eae9df9a3a2f916fb0e8a431a
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-02 14:02:20 -0600

    Used component Card to display Films.

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -1,27 +1,29 @@
 
 import React from 'react';
+import Card from './Card';
 
 const FilmsList = ({ films }) => {
   return (
     <div>
-      <tbody>
-        <tr>
-          <td>id</td>
-          <td>title</td>
-          <td>director</td>
-          <td>producer</td>
-        </tr>
-        {films.map((value, key) => (
-          <tr>
-            <td>{key + 1}</td>
-            <td>{value.title}</td>
-            <td>{value.director}</td>
-            <td>{value.producer}</td>
-          </tr>
-        ))}
-      </tbody>
+      {
+        films.map((cards, i) => {
+          return (
+            <div>
+              <Card
+              title={films[i].title}
+              description={films[i].description}
+              director={films[i].director}
+              producer={films[i].producer}
+              release_date={films[i].release_date}
+              rt_score={films[i].rt_score}
+              />
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
+
 
 export default FilmsList;
