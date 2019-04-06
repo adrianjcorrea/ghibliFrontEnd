b7cf6b9388407939aca4c5a39d38139406cc05c5
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-05 21:53:48 -0600

    Added error component if user goes to undefined URL

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from'../App.js';
 import Films from'./Films.jsx';
+import Error from'./Error.jsx';
 
 class Navigation extends Component {
 render() {
     return(
      <BrowserRouter>
-       <div>
-         <Route path="/" component={App} />
+       <Switch>
+         <Route path="/" component={App} exact />
          <Route path="/Films" component={Films} />
-       </div>
+         <Route component={Error} />
+       </Switch>
      </BrowserRouter>
      );
  }
