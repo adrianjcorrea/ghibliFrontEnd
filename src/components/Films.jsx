70023ce617b32127a48e9169db17613dbf7f9800
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-05 21:38:59 -0600

    Added routing and created Films Component.

---
+++ src/components/Films.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Films extends React.Component{
+
+  render(){
+    return(
+      <h1>See All Filmz</h1>
+    )
+  }
+}
+export default Films;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import FilmsList from './FilmsList.jsx';
-import SearchBox from './SearchBox.jsx';
-import Scroll from './Scroll.jsx';
+
 import App from'../App.js';
+import Films from'./Films.jsx';
 
 class Navigation extends Component {
 render() {
     return(
-     <h1>Hello</h1>
+     <BrowserRouter>
+       <div>
+         <Route path="/" component={App} />
+         <Route path="/Movies" component={Films} />
+       </div>
+     </BrowserRouter>
      );
  }
 }
