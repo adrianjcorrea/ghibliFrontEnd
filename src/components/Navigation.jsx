a3ccc11f09dda871a8ec722a5200f673ce518a80
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-05 21:26:48 -0600

    Refactored code in navigation.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import FilmsList from './components/FilmsList.jsx';
-import SearchBox from './components/SearchBox.jsx';
-import Scroll from './components/Scroll.jsx';
-import App from'../App.jsx'
+import FilmsList from './FilmsList.jsx';
+import SearchBox from './SearchBox.jsx';
+import Scroll from './Scroll.jsx';
+import App from'../App.js'
 
 class Navigation extends Component {
 render() {
     return(
-
+     <h1>Hello</h1>
      );
  }
 }
